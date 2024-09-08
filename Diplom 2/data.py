0021f5bbf5060dcedc87e3b3d108560ca15b3112
Alexander Baranov <59284280+Bars0013@users.noreply.github.com>
2024-09-09 00:08:25 +0300

    Add files via upload

---
+++ Diplom 2/data.py
@@ -0,0 +1,23 @@
+from faker import Faker
+
+fake = Faker()
+
+
+class GenerateUserCredentials:
+    email = ('buntester' + fake.lexify(text='????????????') + '@bunstester.com').lower()
+    password = fake.lexify(text='????????????')
+    name = fake.name()
+
+
+class ExistentUserCredentials:
+    email = 'dj-bars@buntester.com'
+    wrong_email = 'dj-bars666@buntester.com'
+    password = 'buntesterpass'
+    wrong_password = 'notbuntesterpass666'
+    name = 'buntester'
+
+
+class IngredientsData:
+    BUN = '61c0c5a71d1f82001bdaaa6d'
+    SAUCE = '61c0c5a71d1f82001bdaaa75'
+    FILLER = '61c0c5a71d1f82001bdaaa78'
